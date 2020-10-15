aa14bd42ca2d9045c5ec13815e0dbaac624ea1ac
Paul Duschek <Paul.Duschek@HTL-Wels.at>
2020-10-15 12:20:49 +0200

    added JavaFX lib, VM Options; added Override Method start

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,19 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /**
  * @author Paul Duschek
  * @version 1.0, 15.10.2020
  */
 
-public class Main {
+public class Main extends Application {
 
     public static void main(String[] args) {
-        
+
     }
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+    }
 }
